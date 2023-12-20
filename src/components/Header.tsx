749ec7dd6a4c3f285b3719e8c04c9c6fb342b894
2En0 <zeno3618@gmail.com>
2023-12-21 02:28:26 +0900

    feat: add mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,67 @@
+"use client";
 /* eslint-disable @next/next/no-img-element */
 import { HEADER_LINKS } from "@/config";
-import { FC } from "react";
+import { FC, useState } from "react";
+
+import { CiMenuFries } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 
 const Header: FC = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
-    <div className="py-[23px] w-full flex items-center justify-center">
-      <div className="w-full max-w-[1266px] px-[15px] flex items-center justify-between">
-        <div className="flex items-center justify-center gap-[20px] cursor-pointer">
-          <img alt="/imgs/logo.png" src="/imgs/logo.png" />
-          <p className="uppercase text-white text-[25px] logofont">shaga</p>
+    <>
+      <div className="py-[23px] w-full flex items-center justify-center">
+        <div className="w-full max-w-[1266px] px-[15px] flex items-center justify-between">
+          <div className="flex items-center justify-center md:gap-[20px] gap-3 cursor-pointer">
+            <img alt="/imgs/logo.png" src="/imgs/logo.png" />
+            <p className="uppercase text-white md:text-[25px] text-[20px] logofont">
+              shaga
+            </p>
+            <div className="rounded-[17px] border-[1px] border-[#29B080] py-[2px] px-[45px] text-center bg-[#162724] lg:block hidden">
+              <span className="text-[#29B080] text-[15px] font-semibold">
+                Hyperdrive Winners
+              </span>
+              <p className="font-semibold text-white outline-1 outline-black text-[24px]">
+                DePin Track
+              </p>
+            </div>
+          </div>
+          <div className="lg:gap-[42px] gap-[30px] items-center justify-center flex">
+            {HEADER_LINKS.map((item, index) => (
+              <li
+                className="text-[18px] text-[#929298] list-none hidden md:block cursor-pointer"
+                key={index}
+              >
+                {item.title}
+              </li>
+            ))}
+
+            <button className="bg-[#29B080] rounded-[10px] text-white text-[18px] px-[21px] py-3">
+              Join Waitlist
+            </button>
+          </div>
+          <div
+            className="flex justify-center cursor-pointer item-center lg:hidden z-[9999]"
+            onClick={() => setShowMobileMenu(!showMobileMenu)}
+          >
+            {showMobileMenu ? (
+              <IoMdClose color="white" size="30" />
+            ) : (
+              <CiMenuFries color="white" size="30" />
+            )}
+          </div>
+        </div>
+      </div>
+      {showMobileMenu && (
+        <div className="fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-20 backdrop-blur-md z-[9998] items-center justify-center flex flex-col gap-10 lg:hidden">
+          {HEADER_LINKS.map((item, index) => (
+            <li
+              className="text-[30px] text-white list-none cursor-pointer"
+              key={index}
+            >
+              {item.title}
+            </li>
+          ))}
           <div className="rounded-[17px] border-[1px] border-[#29B080] py-[2px] px-[45px] text-center bg-[#162724]">
             <span className="text-[#29B080] text-[15px] font-semibold">
               Hyperdrive Winners
@@ -18,22 +71,8 @@ const Header: FC = () => {
             </p>
           </div>
         </div>
-        <div className="flex gap-[42px] items-center justify-center">
-          {HEADER_LINKS.map((item, index) => (
-            <li
-              className="text-[18px] text-[#929298] list-none hidden md:block cursor-pointer"
-              key={index}
-            >
-              {item.title}
-            </li>
-          ))}
-
-          <button className="bg-[#29B080] rounded-[10px] text-white text-[18px] px-[21px] py-3">
-            Join Waitlist
-          </button>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
