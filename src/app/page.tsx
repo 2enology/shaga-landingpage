119df0f58ccbdcf29ab5a6b9285507740a003cda
2En0 <zeno3618@gmail.com>
2023-12-21 08:15:47 +0900

    fix: update css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,30 @@ import Advisor from "@/components/Advisor";
 import Homepage from "@/components/Homepage";
 import InfiniteSlider from "@/components/InfiniteSlider";
 import MainLayout from "@/layout/MainLayout";
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <MainLayout>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          ></meta>
+          <link
+            href="https://fonts.googleapis.com/css?family=Outfit"
+            rel="stylesheet"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat"
+            rel="stylesheet"
+          ></link>
+          <link rel="shortcut icon" href="/favicon.png"></link>
+          <title>SGN</title>
+        </Head>
+
         <Homepage />
         <InfiniteSlider />
         <About />
